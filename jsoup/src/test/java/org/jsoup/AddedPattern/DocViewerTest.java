71da3b1a0cf941aa5a14ad53eace1a3f3fe1c1b3
HyeonJe <33957389+HyeonJe@users.noreply.github.com>
2019-12-05 21:07:05 +0900

    테스트 케이스 수정

--- jsoup/src/test/java/org/jsoup/AddedPattern/DocViewerTest.java
+++ jsoup/src/test/java/org/jsoup/AddedPattern/DocViewerTest.java
@@ -1,10 +1,30 @@
 package org.jsoup.AddedPattern;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class DocViewerTest {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+        NaverNews naver = new NaverNews();
+        DaumNews daum = new DaumNews();
+        ArrayList<Document> array = new ArrayList<Document>();
+        array.add(doc);
+        array.add(naver.getDoc());
+        array.add(daum.getDoc());
 
+        DocViewer doc_view = new DocViewer(array);
+        //doc_view.makeView();
+
+        //Doc_Viewer view = new Doc_Viewer(naver.getDoc());
+
+    }
 }
